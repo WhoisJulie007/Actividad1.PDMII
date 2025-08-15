b58fe64ed9f12e6b83cee8a6c9530eea665e265c
Julia <maydeli.castan@iest.edu.mx>
2025-08-15 17:47:23 -0600

    Initial Commit

---
+++ Actividades/ActividadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActividadesApp.swift
+//  Actividades
+//
+//  Created by WIN603 on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ActividadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
